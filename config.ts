9e47ea5663fa0472d1213540bf9571ea9beb77ae
wafisharif <wafisharif0@gmail.com>
2026-01-01 20:03:37 -0500

    Update stop_ids in config for subway and bus

--- config.ts
+++ config.ts
@@ -1,21 +1,26 @@
+// config.ts
 export const config: { subway: Array<StopConfig>; bus: Array<StopConfig> } = {
   subway: [
     {
-      stop_ids: ["405S"],
+      stop_ids: ["G05S"],
       walk_time: 10,
     },
     {
-      stop_ids: ["D03S"],
+      stop_ids: ["F03S"],
       walk_time: 14,
     },
   ],
   bus: [
     {
-      stop_ids: ["100017", "103400"],
+      stop_ids: ["553126"],
       walk_time: 3,
     },
     {
-      stop_ids: ["100723"],
+      stop_ids: ["503953"],
+      walk_time: 3,
+    },
+    {
+      stop_ids: ["550036", "553036"],
       walk_time: 3,
     },
   ],
